778f216aacbabe6e23ddcff3b15fa92c1c0562c1
Rafael862 <84231174+Rafael862@users.noreply.github.com>
2023-03-23 22:55:28 -0300

    finalizado aplicação

--- script.js
+++ script.js
@@ -6,36 +6,29 @@ const modalMessage = document.querySelector('.modal .title span')
 const modalBtnClose = document.querySelector('.modal button.close')
 const alertError = document.querySelector('.alert-error')
 
-
-
 modalBtnClose.addEventListener('click', close)
-
-
 
 form.onsubmit = (event) => { 
     const weight = inputWeight.value
     const height = inputHeight.value
     const result = IMC(weight, height)
-
-    if (Number(weight.value) && Number(height.value) !== Number){
-        alert('não é número')
+    if (result == 'NaN'){
+        error()
     } else {
-        alert('é número')
+        modalMessage.innerHTML = `Seu IMC é de ${result}`
+        modalWrapper.classList.add('open')
+        alertError.classList.remove('open')
     }
-    modalMessage.innerHTML = `Seu IMC é de ${result}`
-    modalWrapper.classList.add('open')
 }
-
 
 function close (){
     modalWrapper.classList.remove('open')
 }
 
-
 function IMC(weight, height){
-    return (weight / ((height / 100) ** 2)).toFixed(2)
+        return (weight / ((height / 100) ** 2)).toFixed(2)
 }
 
 function error(){
-    alertError.classList.add('open')
-}
+        alertError.classList.add('open')
+}  
